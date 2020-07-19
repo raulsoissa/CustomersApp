d5f0f5f5d359d1a71ecafb02d1901617f74f145c
raul soissa <raulsoissa@gmail.com>
2020-07-18 20:46:33 -0400

    final video 177

--- src/actions/fetchCustomers.js
+++ src/actions/fetchCustomers.js
@@ -1,23 +1,7 @@
 import { FETCH_CUSTOMERS } from '../constants';
 import { createAction } from 'redux-actions';
 
-const customers = [
-    {
-        "rut": "20200489-k",
-        "name": "Raúl Soissa",
-        "age": 21
-    },
-    {
-        "rut": "11289793-3",
-        "name": "Elsa Rodríguez",
-        "age": 51
-    },
-    {
-        "rut": "27000000-5",
-        "name": "Juan Perez",
-        "age": 45
-    }
-];
+const url = '  http://localhost:3001/customers';
+const apiFetchCustomers = () => fetch(url).then(v => v.json());
 
-
-export const fetchCustomers = createAction(FETCH_CUSTOMERS, () => customers);
+export const fetchCustomers = createAction(FETCH_CUSTOMERS, apiFetchCustomers);
