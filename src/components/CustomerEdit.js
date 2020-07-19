7f2d916bfdd190e4d29461fec8d2f3dce2f14fad
raul soissa <raulsoissa@gmail.com>
2020-07-19 10:05:27 -0400

    final video 192

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -4,6 +4,19 @@ import { reduxForm, Field } from 'redux-form';
 //import { connect } from 'react-redux'
 import { setPropsAsInitial } from '../helpers/setPropsAsInitial';
 
+const isRequired = value => (
+    !value && "Este campo es requerido"
+);
+
+const MyField = ({input, meta}) => (
+    <div className="">
+        <input {...input} type="text"/>
+        {
+            meta.touched && meta.error && <span>{meta.error}</span>
+        }
+    </div>
+);
+
 const CustomerEdit = ({ name, rut, age}) => {
     return (
         <div className="">
@@ -11,15 +24,15 @@ const CustomerEdit = ({ name, rut, age}) => {
             <form action="">
                 <div>
                     <label htmlFor="name">Nombre</label>
-                    <Field name="name" component="input" type="text"></Field>
+                    <Field name="name" component={MyField} type="text" validate={isRequired}></Field>
                 </div>
                 <div>
                     <label htmlFor="rut">Rut</label>
-                    <Field name="rut" component="input" type="text"></Field>
+                    <Field name="rut" component="input" type="text" validate={isRequired}></Field>
                 </div>
                 <div>
                     <label htmlFor="age">Edad</label>
-                    <Field name="age" component="input" type="number"></Field>
+                    <Field name="age" component="input" type="number" validate={isRequired}></Field>
                 </div>
             </form>
         </div>
