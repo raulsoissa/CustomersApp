c2b6de1db7beb46e3b0e9cd85d2500ab082397bf
raul soissa <raulsoissa@gmail.com>
2020-07-18 22:01:18 -0400

    final video 184

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -3,16 +3,24 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import AppFrame from '../components/AppFrame';
 import { getCustomerByRut } from '../selectors/customers';
+import { Route } from 'react-router-dom';
 
 class CustomerContainer extends Component {
+
+    renderBody = () => (
+        <Route path="/customers/:rut/edit" children={
+            ({ match }) => (match ? <p>Es edición</p> : <p>No es edición</p> )
+        }/>
+    )
+
+    // <p>Datos del Cliente "{this.props.customer.name}"</p>
+
     render() {
         return (
             <div className="">
                 <AppFrame
                     header={`Cliente ${this.props.rut}`}
-                    body={
-                        <p>Datos del Cliente "{this.props.customer.name}"</p>
-                    }
+                    body={this.renderBody()}
                 />
             </div>
         )
